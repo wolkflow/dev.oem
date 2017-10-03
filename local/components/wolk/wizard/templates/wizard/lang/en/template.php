37179cdebe528475712ad557543ea78e96a28b19
root <i.vorobyev@glyf.ru>
2017-10-03 12:34:37 +0300

    Wizard form structure langs.

---
+++ local/components/wolk/wizard/templates/wizard/lang/en/template.php
@@ -0,0 +1,155 @@
+<?php
+
+$MESS['STEP_STANDS'] = 'Stand type';
+$MESS['STEP_EQUIPMENTS'] = 'Hardware';
+$MESS['STEP_SERVICES'] = 'Services';
+$MESS['STEP_MARKETINGS'] = 'Marketing';
+$MESS['STEP_SKETCH'] = 'Stand layout';
+$MESS['STEP_ORDER'] = 'Order';
+
+$MESS['TYPE_STANDARD'] = 'Standard';
+$MESS['TYPE_INDIVIDUAL'] = 'Individual';
+
+$MESS['TITLE_EQUIPMENT'] = 'Add. equipment';
+$MESS['TITLE_SERVICES'] = 'Services';
+$MESS['TITLE_MARKETING'] = 'Marketing';
+$MESS['TITLE_SKETCH'] = 'Scheme of the stand';
+
+$MESS['BASKET'] = 'Trash';
+$MESS['PREV'] = 'Back';
+$MESS['NEXT'] = 'Forward';
+
+$MESS['SELECTED_INDIVIDUAL_STAND_NOTE'] = 'On request for participation, you have ordered an unequipped space. You can click "Continue" and go to the selection of additional services or you can choose any booth listed below. ';
+
+$MESS['DEADLINE'] = 'The deadline for the submission of the order';
+$MESS['SURCHARGE'] = 'Extra charge of #PERCENT# will be applied to all orders later #DATE#';
+$MESS['SCHEDULE'] = 'Schedule';
+$MESS['WEEKDAY'] = 'e';
+
+$MESS['PRESELECT_STAND_NOTE'] = 'You have already ordered the "Standard Stand" for the application form. You can continue with the chosen type of stand or choose one of the options below. ';
+$MESS['CURRENT_STAND'] = 'Your current stand';
+$MESS['CONTINUE'] = 'continue';
+$MESS['INCLUDES'] = 'Includes';
+
+$MESS['BETTER_STANDART'] = 'Improved Standard';
+$MESS['BETTER_STANDART_NOTE'] = 'For advice on choosing an improved standard, please contact the technical service.';
+
+$MESS['M2'] = 'm <sup> 2 </ sup>';
+
+$MESS['CHOOSEN'] = 'Selected';
+$MESS['CHOOSE'] = 'Select';
+$MESS['NOT_SELECTED'] = 'Not selected';
+
+$MESS['QUANTITY'] = 'Quantity';
+$MESS['MORE'] = 'Add';
+
+$MESS['DATES'] = 'Dates';
+$MESS['TIMES'] = 'Time';
+$MESS['CLEAR'] = 'Clear';
+$MESS['DATERANGE'] = 'Date range';
+
+$MESS['PRICE'] = 'Price';
+
+$MESS['STANDARD_INCLUDES'] = 'The stand includes';
+
+$MESS['COMMENTS'] = 'Comments';
+$MESS['ADDITIONAL_INFO'] = 'More information about your order';
+$MESS['CONFIGURATION'] = 'Your configuration';
+$MESS['SYSTEM_STAND'] = 'System Booth';
+$MESS['AREA'] = 'Area';
+$MESS['WIDTH'] = 'Width';
+$MESS['DEPTH'] = 'Depth';
+$MESS['TYPE'] = 'Type';
+$MESS['EXHIBITION'] = 'Exhibition';
+$MESS['LOCATION'] = 'Place';
+
+
+// Properties.
+$MESS['FILE'] = 'File';
+
+$MESS['COMMENT'] = 'Comment';
+$MESS['COMMENT_PLACEHOLDER'] = 'Here you can describe in more detail';
+
+$MESS['LINK'] = 'Link';
+
+$MESS['COLOR'] = 'Color';
+$MESS['CHANGE_COLOR'] = 'Change color';
+$MESS['CHOOSE_COLOR'] = 'Select color';
+$MESS['CHOOSING_COLOR'] = 'Choose a color';
+$MESS['COLOR_NOTE'] = 'Colors may not exactly match the actual material';
+
+$MESS['FORM_HANDING_DETAILS'] = 'Suspension to ceiling structures';
+$MESS['FORM_HANDING_COMPANY'] = 'Company name';
+$MESS['FORM_HANDING_PAVILION'] = 'Pavilion No.';
+$MESS['FORM_HANDING_HALL'] = 'Room No.';
+$MESS['FORM_HANDING_HALL'] = 'Room No.';
+$MESS['FORM_HANDING_STAND'] = 'Stand No.';
+$MESS['FORM_HANDING_SIZE'] = 'Size';
+$MESS['FORM_HANDING_MATERIAL'] = 'Material';
+$MESS['FORM_HANDING_WEIGHT'] = 'Weight';
+
+
+$MESS['STAND_TYPE'] = 'Type of stand';
+$MESS['SYSTEM_STAND'] = 'System Booth';
+
+$MESS['CHANGE'] = 'Change';
+$MESS['DELETE'] = 'Delete';
+
+$MESS['STAND'] = 'Stand';
+$MESS['PAVILION'] = 'Pavilion';
+
+$MESS['PRICE_TOTAL'] = 'Total';
+$MESS['SURCHRGE'] = 'Extra charge';
+$MESS['PRICE_TOTAL_WITH_VAT'] = 'Total with VAT';
+$MESS['VAT'] = 'VAT';
+$MESS['PRICE_TOTAL_WITH_SURCHARGE'] = 'Total with extra charge';
+
+$MESS['PLACE_ORDER'] = 'Make an order';
+$MESS['PLACE_ORDER_NOT_LOGIN_TEXT'] = 'You need to review the participation rules and log in to place an order. If you already have an account, select "Login" and enter your credentials. If you have not registered yet, please choose "Registration" and fill out the form. ';
+
+$MESS['LOGIN'] = 'Login';
+$MESS['REGISTER'] = 'Registration';
+
+$MESS['USER_LOGIN'] = 'Login';
+$MESS['USER_PASSWORD'] = 'Password';
+
+$MESS['COMPANY_NAME'] = 'Company name';
+$MESS['COMPANY_ADDRESS'] = 'Company Address';
+$MESS['NAME'] = 'Name';
+$MESS['LAST_NAME'] = 'Famiya';
+$MESS['PERSONAL_MOBILE'] = 'Phone';
+$MESS['EMAIL'] = 'E-mail';
+$MESS['CONFIRM_EMAIL'] = 'Confirm e-mail';
+$MESS['USER_CONFIRM_PASSWORD'] = 'Confirm Password';
+$MESS['VAT_ID'] = 'Vat ID';
+
+$MESS['DATERANGE'] = 'Range';
+$MESS['CANCEL'] = 'Cancel';
+
+$MESS['SUCCESS'] = 'Successful!';
+$MESS['ORDER_PLACED'] = 'Order issued';
+$MESS['HOME'] = 'Home';
+$MESS['REVIEW_ORDER'] = 'View Order';
+
+$MESS['FORM_COMPANY'] = 'Company name';
+$MESS['FORM_PAVILION'] = 'Pavilion No.';
+$MESS['FORMM_HALL'] = 'Room No';
+$MESS['FORM_STAND'] = 'Stand No.';
+$MESS['FORM_SIZE'] = 'Size';
+$MESS['FORM_SIZE_DIMENSION'] = 'mm';
+$MESS['FORM_SIZE_HOLDER'] = 'L x W x H';
+$MESS['FORM_METARIAL'] = 'Material';
+$MESS['FORM_WEIGHT'] = 'Weight';
+$MESS['FORM_WEIGHT_DIMENSION'] = 'kg';
+$MESS['FORM_LIST'] = 'Equipment Placement List for Structure';
+$MESS['FORM_LIST_HOLDER'] = 'Please indicate the weight of each element (lighting, advertising equipment, decor elements)';
+$MESS['FORM_MATERIAL'] = 'Material';
+$MESS['FORM_WEIGHT_POINT'] = 'Weight per point';
+$MESS['FORM_WEIGHT_POINT_DIMENSION'] = 'kg';
+$MESS['FORM_HEIGHT'] = 'Height';
+$MESS['FORM_HEIGHT_DIMENSION'] = 'mm';
+$MESS['FORM_WEIGHT_TOTAL'] = 'Final weight';
+$MESS['FORM_WEIGHT_TOTAL_DIMENSION'] = 'kg';
+$MESS['FORM_ASSIGNEE_PROJECT'] = 'Responsible for the structure design';
+$MESS['FORM_ASSIGNEE_MOUNTING'] = 'Responsible for installation work';
+$MESS['FORM_PHONE'] = 'Phone number';
