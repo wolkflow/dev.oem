df1df11b8f88a48fdfd15e404c39307c6bd0409c
root <i.vorobyev@glyf.ru>
2017-10-05 19:45:56 +0300

    Исправление свойства оборудование в стенде.

--- local/modules/wolk.oem/lib/properties/usingequipmentpropertytype.php
+++ local/modules/wolk.oem/lib/properties/usingequipmentpropertytype.php
@@ -17,9 +17,9 @@ class UsingEquipmentPropertyType extends \CIBlockPropertyElementList
 	}
 	
     
-	public static function GetPropertyFieldHtml($arProperty, $value, $strHTMLControlName)
+	public static function GetPropertyFieldHtml($property, $value, $strHTMLControlName)
     {
-        $settings = \CIBlockPropertyElementList::PrepareSettings($arProperty);
+        $settings = \CIBlockPropertyElementList::PrepareSettings($property);
         
         if ($settings["size"] > 1) {
             $size = ' size="'.$settings["size"].'"';
@@ -35,16 +35,19 @@ class UsingEquipmentPropertyType extends \CIBlockPropertyElementList
         
         $selected = false;
         
-        $options = \CIBlockPropertyElementList::GetOptionsHtml($property, array($value["VALUE"]), $selected);
+        $options = \CIBlockPropertyElementList::GetOptionsHtml($property, array($value['VALUE']), $selected);
 
-        $html = '<select style="margin: 15px 0 5px 0;" data-test="test" name="'.$strHTMLControlName["VALUE"].'"'. $size . $width . '>';
+		$html  = '<div style="margin-bottom: 3px;">';
+        $html .= '<select data-test="test" name="'.$strHTMLControlName['VALUE'].'"'. $size . $width . '>';
         if ($property['IS_REQUIRED'] != 'Y') {
-            $html .= '<option value=""' . (!$selected? ' selected': '') . '>' . GetMessage("IBLOCK_PROP_ELEMENT_LIST_NO_VALUE") . '</option>';
+            $html .= '<option value=""' . (!$selected ? ' selected' : '') . '>' . GetMessage('IBLOCK_PROP_ELEMENT_LIST_NO_VALUE') . '</option>';
         }
         $html .= $options;
         $html .= '</select>';
         $html .= '<input type="text" value="' . $value['DESCRIPTION'] . '" name="' . $strHTMLControlName['DESCRIPTION'] . '" /><br/>';
-        
+        $html .= '</div>';
+		
+		
 		return $html;
 	}
 }
